91e2efcf535e35d171c4fd59c490224a050c0832
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2025-07-12 23:16:56 +0400

    Add conditional statements examples: If-Else, If-Else-If, and Switch

---
+++ Module-1/07-Conditional-Statements/01-If-else-Statement.js
@@ -0,0 +1,8 @@
+// If-Else statement in JavaScript
+var marks = 40;
+if(marks >= 50) {
+    console.log("You have passed the exam.");
+}
+else {
+    console.log("You have failed the exam.");
+}
---
+++ Module-1/07-Conditional-Statements/02-If-Else-If-Statement.js
@@ -0,0 +1,14 @@
+// If else if statement in JavaScript
+var marks = 90;
+if(marks >= 90) {
+    console.log("You have achieved an A grade.");
+}
+else if(marks >= 75) {
+    console.log("You have achieved a B grade.");
+}
+else if(marks >= 50) { 
+    console.log("You have achieved a C grade.");
+}
+else {
+    console.log("You have failed the exam.");
+}
