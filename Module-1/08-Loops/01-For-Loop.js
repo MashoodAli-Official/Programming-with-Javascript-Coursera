72cd9cd9ef02349b277ffc9a15676137f9cefffe
Mashood Ali <54344218+MashoodAli-Official@users.noreply.github.com>
2025-07-13 21:06:53 +0400

    Add examples for For Loop and While Loop in JavaScript

---
+++ Module-1/08-Loops/01-For-Loop.js
@@ -0,0 +1,5 @@
+// For Loop in javascript use to iterate over a block of code a number of times. 
+// The following code will print "Hello World" 5 times
+for (let i = 0; i < 5; i++) {
+    console.log("Hello World " + i);
+};
---
+++ Module-1/08-Loops/02-While-Loop.js
@@ -0,0 +1,8 @@
+// In JavaScript, the while loop is a control flow statement that repeatedly executes a block of code as long as a specified condition evaluates to true
+var counter = 3;
+while (counter > 0) {
+    console.log(counter);
+    counter = counter - 1; // Decrement the counter by 1
+};
+console.log("Liftoff!"); // This will execute after the loop ends
+// loops are common way to automate the repetitive tasks in programming
